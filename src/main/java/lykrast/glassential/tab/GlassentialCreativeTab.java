e2111043b0998696e10f0e438a3105d8dd92fce6
Big_Energy <andre90094@gmail.com>
2024-02-23 08:08:03 +0300

    misc refactor

--- src/main/java/lykrast/glassential/tab/GlassentialCreativeTab.java
+++ src/main/java/lykrast/glassential/tab/GlassentialCreativeTab.java
@@ -12,11 +12,11 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.registries.RegisterEvent;
 
 public class GlassentialCreativeTab {
-    public static final ResourceKey<CreativeModeTab> MORE_DOORS = ResourceKey.create(Registries.CREATIVE_MODE_TAB, new ResourceLocation("glassential", Glassential.MODID));
+    public static final ResourceKey<CreativeModeTab> GLASSENTIAL = ResourceKey.create(Registries.CREATIVE_MODE_TAB, new ResourceLocation("glassential", Glassential.MODID));
 
     public static void registerTab(RegisterEvent event) {
         event.register(Registries.CREATIVE_MODE_TAB, helper -> {
-            helper.register(MORE_DOORS, CreativeModeTab.builder().displayItems((flags, output) -> GItems.ITEMS.getEntries().forEach(o -> output.accept(o.get()))).title(Component.translatable("itemGroup.glassential")).icon(() -> { return new ItemStack(GBlocks.GLASS_DARK_ETHEREAL.get()); }).build());
+            helper.register(GLASSENTIAL, CreativeModeTab.builder().displayItems((flags, output) -> GItems.ITEMS.getEntries().forEach(o -> output.accept(o.get()))).title(Component.translatable("itemGroup.glassential")).icon(() -> { return new ItemStack(GBlocks.GLASS_DARK_ETHEREAL.get()); }).build());
 
         });
     }
