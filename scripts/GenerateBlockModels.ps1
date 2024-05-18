52ffb1f3aa9b4356af99477ea3d291a5ca87dc9a
Big_Energy <andre90094@gmail.com>
2024-05-18 11:34:22 +0300

    updated to 1.20.4, support multiplatform (forge/fabric/neoforge)

--- scripts/GenerateBlockModels.ps1
+++
@@ -1,9 +0,0 @@
-﻿$BlockData = Get-Content -Path BlockAPINames.txt
-$ModelDataBlueprint = Get-Content -Path BlockModelBlueprint.txt
-
-foreach($BlockInfo in $BlockData){
-    $ApiName,$DisplayName,$Material,$MaterialColor,$Hardness,$Resistance = $BlockInfo -split '\t'
-    
-    $ModelData = $ModelDataBlueprint.Replace('{API_NAME}', $ApiName)
-    $ModelData | Out-File "Output\models\blocks\$ApiName.json" -Encoding utf8
-}
