2c83c19e0d45606d31859a96041027b5b791aeb1
Big_Energy <andre90094@gmail.com>
2025-09-14 19:05:38 +0300

    fix Tinted Ethereal Glass Pane and Tinted Reverse Ethereal Glass Pane https://github.com/bigenergy/glassential-renewed/issues/45

--- src/main/java/com/github/bigenergy/glassential/blocks/panes/DarkEtherealPaneBlock.java
+++ src/main/java/com/github/bigenergy/glassential/blocks/panes/DarkEtherealPaneBlock.java
@@ -1,10 +1,13 @@
 package com.github.bigenergy.glassential.blocks.panes;
 
 import net.minecraft.ChatFormatting;
+import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.block.state.BlockState;
 
 import java.util.List;
 
@@ -19,5 +22,13 @@ public class DarkEtherealPaneBlock extends EtherealPaneBlock {
     public void appendHoverText(ItemStack pStack, Item.TooltipContext pContext, List<Component> pTooltipComponents, TooltipFlag pTooltipFlag) {
         super.appendHoverText(pStack, pContext, pTooltipComponents, pTooltipFlag);
         pTooltipComponents.add(Component.translatable("tooltip.glassential.dark_pane").withStyle(ChatFormatting.GRAY));
+    }
+
+    protected boolean propagatesSkylightDown(BlockState p_154824_, BlockGetter p_154825_, BlockPos p_154826_) {
+        return false;
+    }
+
+    protected int getLightBlock(BlockState p_154828_, BlockGetter p_154829_, BlockPos p_154830_) {
+        return p_154829_.getMaxLightLevel();
     }
 }
