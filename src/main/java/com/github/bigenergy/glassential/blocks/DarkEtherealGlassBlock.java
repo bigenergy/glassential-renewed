d5af38a91bbb8e8404454397784b484436d87d90
Big_Energy <andre90094@gmail.com>
2026-04-13 22:46:26 +0300

    4.0.0-beta

--- src/main/java/com/github/bigenergy/glassential/blocks/DarkEtherealGlassBlock.java
+++ src/main/java/com/github/bigenergy/glassential/blocks/DarkEtherealGlassBlock.java
@@ -1,8 +1,23 @@
 package com.github.bigenergy.glassential.blocks;
 
-public class DarkEtherealGlassBlock extends EtherealGlassBlock {
+import net.minecraft.ChatFormatting;
+import net.minecraft.core.component.DataComponentGetter;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.item.component.TooltipProvider;
+import org.jspecify.annotations.NonNull;
+
+import java.util.function.Consumer;
+
+public class DarkEtherealGlassBlock extends EtherealGlassBlock implements TooltipProvider {
 
     public DarkEtherealGlassBlock(Properties properties, boolean collidePlayers) {
         super(properties, collidePlayers);
+    }
+
+    @Override
+    public void addToTooltip(Item.@NonNull TooltipContext tooltipContext, Consumer<net.minecraft.network.chat.Component> consumer, @NonNull TooltipFlag tooltipFlag, @NonNull DataComponentGetter dataComponentGetter) {
+        consumer.accept(Component.translatable("tooltip.glassential.dark").withStyle(ChatFormatting.GRAY));
     }
 }
