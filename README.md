1d6c226d36291fa9f39c577b83eecb297b964209
Big_Energy <andre90094@gmail.com>
2026-07-30 03:55:15 +0300

    upd

---
+++ README.md
@@ -0,0 +1,5 @@
+# Glassential Renewed
+
+[CurseForge link](https://curseforge.com/minecraft/mc-mods/glassential-renewed)
+
+Adds glass of various type to your world. Inspired by all the cool glasses in Extra Utilities 2, but comes without all the other stuff.
---
+++ src/main/java/com/bigenergy/glassential/panes/RedstonePaneBlock.java
@@ -0,0 +1,43 @@
+package com.bigenergy.glassential.panes;
+
+import net.minecraft.ChatFormatting;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.block.IronBarsBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+
+import javax.annotation.Nullable;
+import java.util.List;
+
+public class RedstonePaneBlock extends IronBarsBlock{
+
+    public RedstonePaneBlock(Properties properties) {
+        super(properties);
+    }
+
+    @Override
+    public boolean isSignalSource(BlockState state) {
+        return true;
+    }
+
+    @Override
+    public int getSignal(BlockState state, BlockGetter world, BlockPos pos, Direction direction) {
+        return 15;
+    }
+
+    @Override
+    @OnlyIn(Dist.CLIENT)
+    public void appendHoverText(ItemStack stack, @Nullable BlockGetter worldIn, List<Component> tooltip, TooltipFlag flagIn) {
+        super.appendHoverText(stack, worldIn, tooltip, flagIn);
+        tooltip.add(Component.translatable("tooltip.glassential.pane_frameless").withStyle(ChatFormatting.GRAY));
+        tooltip.add(Component.translatable("tooltip.glassential.redstone").withStyle(ChatFormatting.RED));
+    }
+
+
+}
